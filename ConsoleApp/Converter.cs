93a1adea0d6198aff8b087f03713e6339ecf70a5
Kay van der Lans <kay.van.der.lans@netive.nl>
2024-10-29 20:12:18 +0100

    added simple poc converter

---
+++ ConsoleApp/Converter.cs
@@ -0,0 +1,60 @@
+using System.Text.Json;
+using System.Xml.Linq;
+
+namespace AMS2ToApexRivals;
+
+public class Converter(string inputPath, string outputDir, string defaultValue = "Random")
+{
+	public void Convert() 
+	{
+		var options = new JsonSerializerOptions { WriteIndented = true };
+
+		Directory.CreateDirectory(outputDir);
+
+		var root = XElement.Load(inputPath);
+		foreach (var driver in root.Elements("driver")) 
+		{ 
+			var name = driver.Element("name")?.Value ?? "Unknown";
+            var country = driver.Element("country")?.Value ?? "Unknown";
+
+			var ams2 = new 
+			{
+				Race_Skill = GetValueOrDefault(driver, "race_skill"),
+				Qualifying_Skill = GetValueOrDefault(driver, "qualifying_skill"),
+				Aggression = GetValueOrDefault(driver, "aggression"),
+				Defending = GetValueOrDefault(driver, "defending"),
+				Stamina = GetValueOrDefault(driver, "stamina"),
+				Consistency = GetValueOrDefault(driver, "consistency"),
+				Tyre_Change = GetValueOrDefault(driver, "weather_tyre_changes"),
+				Start_Reaction = GetValueOrDefault(driver, "start_reactions"),
+				Wet_Skill = GetValueOrDefault(driver, "wet_skill"),
+				Tyre_Management = GetValueOrDefault(driver, "tyre_management"),
+				Fuel_Management = GetValueOrDefault(driver, "fuel_management"),
+				Blue_Flag = GetValueOrDefault(driver, "blue_flag_conceding"),
+				Mistake_Avoidance = GetValueOrDefault(driver, "avoidance_of_mistakes"),
+				Forced_Avoidance = GetValueOrDefault(driver, "avoidance_of_forced_mistakes")
+			};
+
+			var json = new 
+			{
+				Name = name,
+				Country = country,
+				AMS2 = ams2,
+				AC = new { Strength = "Random", Aggressivity = "Random" }
+			};
+
+			var jsonOutput = JsonSerializer.Serialize(json, options).Replace('_', ' ');
+			var outputPath = Path.Combine(outputDir, name);
+			Directory.CreateDirectory(outputPath);
+			outputPath = Path.Combine(outputPath, $"{name}.json");
+			File.WriteAllText(outputPath, jsonOutput);
+			Console.WriteLine($"Saved JSON for {name} at {outputPath}");
+		}
+
+	}
+
+	private string GetValueOrDefault(XElement root, string element) 
+	{
+		return root.Element(element)?.Value ?? defaultValue;
+	}
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// Temporary while I'm working on my mac.
+
+var input = Path.Combine(Environment.CurrentDirectory, "test_input", "inputTest.xml");
+var dir = Path.Combine(Environment.CurrentDirectory, "test_output");
+new AMS2ToApexRivals.Converter(input, dir).Convert();
